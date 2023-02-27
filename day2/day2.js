1fb421f58e0bc7b57d95ee391d46f3acebff917d
sabatms <saba.tms15@gmail.com>
2023-02-27 11:02:22 -0800

    add day2.js file

---
+++ day2/day2.js
@@ -0,0 +1,22 @@
+let firstname = "saba";
+
+console.log(firstname);
+
+
+firstname = "ali";
+console.log(firstname);
+
+let esme ="ali";
+let lastname='shirazi';
+let age =23;
+
+console.log( esme, lastname,age );
+
+const access ="yes";
+console.log(access);
+
+var filed="Nursing";
+console.log(filed);
+
+filed="Programing";
+console.log(filed);
