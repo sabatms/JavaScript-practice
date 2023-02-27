862f4bb536c6004c7b5e7631bb296eb331af552a
sabatms <saba.tms15@gmail.com>
2023-02-27 12:19:59 -0800

    add day3.js file

---
+++ day3/day3.js
@@ -0,0 +1,23 @@
+/*var myVar = 10;
+let myLet = 20;
+
+var myVar =1000;
+myLet =2000;
+
+
+{
+          var myVar=1000;
+          let myLet =2000;
+}
+
+console.log(myVar);
+console.log(myLet);
+*/
+
+console.log(myVar);
+var myVar=10;
+console.log(myVar);
+
+//console.log(myLet);
+let myLet=20;
+console.log(myLet);
