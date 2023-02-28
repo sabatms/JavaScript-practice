b81caed746bdfad12e501751885b431790e3b807
sabatms <saba.tms15@gmail.com>
2023-02-28 09:51:28 -0800

    add day4.js file

---
+++ day4/day4.js
@@ -0,0 +1,39 @@
+let firstname = "mehran";
+let lastname = "tarif";
+let age = 24;
+
+console.log(firstname,lastname,age);
+
+const access = "yes";
+console.log(access);
+
+var filed ="Nursing";
+console.log(filed);
+
+filed = "developer";
+console.log(filed);
+
+/*var myVar = 20;
+let myLet = 10;
+
+var myVar = 2000;
+let myLet = 2000;
+myLet = 1000;
+
+{
+          var myVar = 2000;  
+          let myLet = 1000;
+          console.log(myLet); 
+}
+
+console.log(myVar);
+console.log(myLet);
+*/
+
+console.log(myVar);
+var myVar =10;
+console.log(myVar);
+
+/*console.log(myLet);
+ let myLet=20;
+console.log(myLet);*/
