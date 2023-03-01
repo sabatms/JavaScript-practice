0e2dbfc825b1988ed1eb29c5673c53eccde1c94c
sabatms <saba.tms15@gmail.com>
2023-03-01 12:54:48 -0800

    add day5.js file

---
+++ day5/day5.js
@@ -0,0 +1,8 @@
+let firstName="saba";
+let lastName = "tahmasebi";
+let age = 24;
+
+//let data = 'this is ' +firstName +" "+lastName+" and she is " + age + " years old! " 
+
+let data = 
+console.log(data);
