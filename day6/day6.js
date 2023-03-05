ee5d7312371f3feb5aedead86207e88b262d645e
sabatms <saba.tms15@gmail.com>
2023-03-04 23:46:01 -0800

    add day6.js update file

--- day6/day6.js
+++ day6/day6.js
@@ -1,1 +1,14 @@
-let unm1=3
+let data="JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat."
+//console.log(data);
+
+//console.log(data.length);
+
+//console.log(data.slice(0,10));
+data=data.toLowerCase()
+//console.log(data.includes("javascript"))
+
+//console.log(data.toLowerCase())
+
+//console.log(data.search("js"));
+
+console.log(data.replace("javascript",'js'))
