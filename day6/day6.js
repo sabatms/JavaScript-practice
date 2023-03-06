f352b8614b103c6a0df5cf8a28beca91ead03887
sabatms <saba.tms15@gmail.com>
2023-03-06 00:03:08 -0800

    add update day6.js file

--- day6/day6.js
+++ day6/day6.js
@@ -1,19 +1,14 @@
-//let data="JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat."
-//console.log(data);
+let data="JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat."
+console.log(data);
 
-//console.log(data.length);
+console.log(data.length);
 
-//console.log(data.slice(0,10));
-//data=data.toLowerCase()
-//console.log(data.includes("javascript"))
+console.log(data.slice(0,10));
+data=data.toLowerCase()
+console.log(data.includes("javascript"))
 
-//console.log(data.toLowerCase())
+console.log(data.toLowerCase())
 
-//console.log(data.search("js"));
+console.log(data.search("js"));
 
-//console.log(data.replace("javascript",'js'));
-
-age = 23;
-console.log(age);
-age == 23;
-console.log(age)
+console.log(data.replace("javascript",'js'));
